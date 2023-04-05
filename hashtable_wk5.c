1f1d630d92e31118fdc85bb47dbbe6d17ff5b159
Anh Doan <Anh.Doan@utas.edu.au>
2023-04-05 17:34:03 +1000

    final implementation

--- hashtable_wk5.c
+++ hashtable_wk5.c
@@ -6,10 +6,10 @@ HashTable create_hashtable(int n) {
 	HashTable newtable;
 
 	// TODO: allocate memory for array and init lists
-	newtable.size = n;
-	newtable.table = malloc(n * sizeof * newtable.table);
+	newtable.size = n; 
+	newtable.table = malloc(n * sizeof(List));
 	for (int i = 0; i < n; i++) {
-		newtable.table[i] = new_list();
+		newtable.table[i] = new_list();//create new list for each row
 	}
 
 	return newtable;
@@ -28,16 +28,17 @@ int hash(String key, int size) {
 
 void hash_insert(HashTable* self, String key) {
 	// TODO
-
+	int row = 0; 
 	// 1. find the list to insert into using hash
-	int i = hash(key, self->size);
+	row = hash(key, self->size);
 
 	// 2. call list function to insert into that list
-	insert_at_front(&(self->table[i]), key);
+	insert_at_front(&(self->table[row]), key);
 }
 
 void hash_remove(HashTable* self, String key) {
-	// TODO
+	int i = hash(key, self->size);//get the key
+	delete_from_list(&self->table[i], key); //delete
 
 }
 
@@ -50,4 +51,10 @@ void hash_print(HashTable* self) {
 
 void hash_destroy(HashTable* self) {
 	// TODO
+	// iterate over each bucket in the table
+	for (int i = 0; i < self->size; i++) {
+		destroy_list(&(self->table[i]));// destroy the list in the bucket
+	}
+	// free the memory allocated for the table itself
+	free(self->table);
 }
--- week5.c
+++ week5.c
@@ -6,7 +6,7 @@
 
 int main() {
 	int command = 1;
-	//HashTable table = create_hashtable(11);
+	HashTable table = create_hashtable(11);
 	List strings = new_list();
 	char buffer[100];
 
@@ -19,21 +19,29 @@ int main() {
 			printf("Enter the string to insert");
 			printf("\n");
 			scanf_s("%s", buffer, 100);
-			//hash_insert(&table, buffer);
-
+			hash_insert(&table, buffer);
 			insert_at_front(&strings, buffer);
 			break;
 		case 2: 
 			printf("Enter the string to delete");
 			printf("\n");
 			scanf_s("%s", buffer, 100);
+			hash_remove(&table, buffer);
 			delete_from_list(&strings, buffer);
 			break;
 		case 3: 
 			print_list(&strings);
+			hash_print(&table);
+			break;
+		case 0:
+			break;
+		default: 
+			printf("Error!\n");
 			break;
 		}
 		printf("\n");
 	}
+	//free memory
 	destroy_list(&strings);
+	hash_destroy(&table);
 }
